97b91801389b40e602f0225b259a3dc62a0df50f
MuratOldman <muratkara15102@gmail.com>
2022-09-16 23:26:12 -0400

    I added validation part

--- src/main/java/com/company/controller/EmployeeController.java
+++ src/main/java/com/company/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.company.controller;
 
+import com.company.bootstrap.DataGenerator;
 import com.company.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,7 +16,19 @@ public class EmployeeController {
     @GetMapping("/register")
     public String createEmployee(Model model){
       model.addAttribute("employee",new Employee());
+      model.addAttribute("states", DataGenerator.getAllStates());
         return "/employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String getEmployeeList(@ModelAttribute("employee") Employee employee, Model model){
+
+        DataGenerator.saveEmployee(employee);
+
+        model.addAttribute("employeeList",DataGenerator.readAllEmployees());
+
+
+        return "/employee/employee-list";
     }
 
 }
