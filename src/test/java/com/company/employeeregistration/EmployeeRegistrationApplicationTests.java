dc51e43c0675a20aa12d5742da829b832b987cf0
MuratOldman <muratkara15102@gmail.com>
2022-09-16 22:05:16 -0400

    Initial commit

---
+++ src/test/java/com/company/employeeregistration/EmployeeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.employeeregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
